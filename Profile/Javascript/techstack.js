afb2a476516dde902d7b109452fa7a1e92006dbd
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-03-16 08:00:26 +0530

    completed about

--- Profile/Javascript/techstack.js
+++ Profile/Javascript/techstack.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
     AOS.init({ duration: 1000, easing: "ease-in-out", once: true });
 });
 
-<!-- AOS Library -->
+// AOS Library 
 document.addEventListener("DOMContentLoaded", function() {
         AOS.init({ duration: 1000, easing: "ease-in-out", once: true });
     });
